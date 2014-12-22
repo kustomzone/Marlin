2ca88687206f2ab9dad4da6868ab2077114c4d75
Luis E Alvarado <alvaradorocks@gmail.com>
2014-12-21 20:04:55 -0600

    Re-enable after merge.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -350,7 +350,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define Z_MAX_LENGTH (Z_MAX_POS - Z_MIN_POS)
 //============================= Bed Auto Leveling ===========================
 
-//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
+#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
 #define Z_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #ifdef ENABLE_AUTO_BED_LEVELING
@@ -402,7 +402,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER   -27 //-25
   #define Y_PROBE_OFFSET_FROM_EXTRUDER   -60 //-29
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER   -1.40 //-12.35 (Old mount design: -1.50)
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER   -1.50 //-12.35 (Old mount design: -1.50)
 
   #define Z_RAISE_BEFORE_HOMING 10       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
