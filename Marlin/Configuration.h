11626971e94274b721f846434e2b541516db9a72
Luis E Alvarado <alvaradorocks@gmail.com>
2015-01-07 23:05:44 -0600

    PID Autotune: M303 E0 S240 C8

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -89,7 +89,7 @@
 // 10 is 100k RS thermistor 198-961 (4.7k pullup)
 // 11 is 100k beta 3950 1% thermistor (4.7k pullup)
 // 12 is 100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup) (calibrated for Makibox hot bed)
-// 13 is 100k Hisens 3950  1% up to 300Â°C for hotend "Simple ONE " & "Hotend "All In ONE" 
+// 13 is 100k Hisens 3950  1% up to 300¡C for hotend "Simple ONE " & "Hotend "All In ONE" 
 // 20 is the PT100 circuit found in the Ultimainboard V2.x
 // 60 is 100k Maker's Tool Works Kapton Bed Thermistor beta=3950
 //
@@ -175,9 +175,9 @@
 //    #define  DEFAULT_Kd 440
 
 // Phoenix Ez3D
-    #define DEFAULT_Kp 10.92
-    #define DEFAULT_Ki 0.47
-    #define DEFAULT_Kd 62.82
+    #define DEFAULT_Kp 14.29
+    #define DEFAULT_Ki 0.78
+    #define DEFAULT_Kd 65.86
 #endif // PIDTEMP
 
 // Bed Temperature Control
